d146cdb95e64d237855d8cd7cc017cbabc2c4de4
arlaspresident <jankovic4901@gmail.com>
2025-04-09 11:16:16 +0200

    lagt till installationsskript för databasen

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS courses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    coursecode TEXT NOT NULL,
+    coursename TEXT NOT NULL,
+    syllabus TEXT NOT NULL,
+    progression TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
